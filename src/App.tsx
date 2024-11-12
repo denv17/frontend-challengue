ab9599b4e817d00dd46b69ec5428015311e03b3c
Denis Ventura <dventura017@gmail.com>
2024-11-12 00:05:09 -0500

    feat: add colors variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>React TypeScript Template</h1>
-      <Button label="Click me" variant="primary" onClick={() => console.log('Button clicked')} />
+      <Button label="Click me" onClick={() => console.log('Button clicked')} />
     </div>
   )
 }
